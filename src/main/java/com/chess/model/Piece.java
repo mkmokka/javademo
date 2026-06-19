10873f9da32e0712823f2a4c764a5069e2731a13
mkmokka <mkmokka12@gmail.com>
2026-06-19 12:27:59 +0600

    Update Piece.java

--- src/main/java/com/chess/model/Piece.java
+++ src/main/java/com/chess/model/Piece.java
@@ -1,41 +1,14 @@
 package com.chess.model;
 
 public class Piece {
-    private final PieceType type;
-    private final PieceColor color;
+    private PieceType type;
+    private PieceColor color;
 
     public Piece(PieceType type, PieceColor color) {
         this.type = type;
         this.color = color;
     }
 
-    public PieceType getType() {
-        return type;
-    }
-
-    public PieceColor getColor() {
-        return color;
-    }
-
-    public String getSymbol() {
-        if (color == PieceColor.WHITE) {
-            return switch (type) {
-                case PAWN -> "♙";
-                case ROOK -> "♖";
-                case KNIGHT -> "♘";
-                case BISHOP -> "♗";
-                case QUEEN -> "♕";
-                case KING -> "♔";
-            };
-        } else {
-            return switch (type) {
-                case PAWN -> "♟";
-                case ROOK -> "♜";
-                case KNIGHT -> "♞";
-                case BISHOP -> "♝";
-                case QUEEN -> "♛";
-                case KING -> "♚";
-            };
-        }
-    }
+    public PieceType getType() { return type; }
+    public PieceColor getColor() { return color; }
 }
