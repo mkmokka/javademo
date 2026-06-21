d3f55461836ee3c4710785fa1000c6e7c651b49f
mkmokka <mkmokka12@gmail.com>
2026-06-21 21:33:23 +0600

    Update UserMoveStrategy.java

---
+++ src/main/java/com/chess/patterns/strategy/UserMoveStrategy.java
@@ -0,0 +1,8 @@
+package com.chess.patterns.strategy;
+
+import com.chess.model.Board;
+import com.chess.model.Move;
+
+public class UserMoveStrategy implements MoveStrategy {
+    public Move calculateMove(Board board) { return null; } // প্লেয়ার নিজে ইনপুট দেয়
+}
