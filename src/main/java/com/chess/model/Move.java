220b646bb66cf91dfe47fc18f49ec81814fb1d55
mkmokka <mkmokka12@gmail.com>
2026-06-21 22:11:39 +0600

    Update Move.java

--- src/main/java/com/chess/model/Move.java
+++ src/main/java/com/chess/model/Move.java
@@ -1,3 +1,24 @@
 package com.chess.model;
 
-public record Move(Position from, Position to) {}
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Move {
+    private Position from;
+    private Position to;
+
+    public Move() {}
+
+    public Move(Position from, Position to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @JsonProperty("from")
+    public Position from() { return from; }
+
+    @JsonProperty("to")
+    public Position to() { return to; }
+
+    public void setFrom(Position from) { this.from = from; }
+    public void setTo(Position to) { this.to = to; }
+}
