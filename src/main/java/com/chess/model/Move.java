efadb846c8398df6fb8bc3a92d66380624d67cf2
mkmokka <mkmokka12@gmail.com>
2026-06-22 02:12:33 +0600

    Update Move.java

--- src/main/java/com/chess/model/Move.java
+++ src/main/java/com/chess/model/Move.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Move {
     private Position from;
     private Position to;
+    private String promotionType; // "QUEEN", "ROOK", etc.
 
     public Move() {}
 
@@ -19,6 +20,10 @@ public class Move {
     @JsonProperty("to")
     public Position to() { return to; }
 
+    @JsonProperty("promotionType")
+    public String getPromotionType() { return promotionType; }
+
     public void setFrom(Position from) { this.from = from; }
     public void setTo(Position to) { this.to = to; }
+    public void setPromotionType(String promotionType) { this.promotionType = promotionType; }
 }
