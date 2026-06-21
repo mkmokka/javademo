9e77dfc466c6de66b486dd35edd2c2ebed9fc44e
mkmokka <mkmokka12@gmail.com>
2026-06-21 22:09:58 +0600

    Update Position.java

--- src/main/java/com/chess/model/Position.java
+++ src/main/java/com/chess/model/Position.java
@@ -1,6 +1,28 @@
 package com.chess.model;
 
-public record Position(int row, int col) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Position {
+    private int row;
+    private int col;
+
+    // Default constructor for Jackson parsing
+    public Position() {}
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    @JsonProperty("row")
+    public int row() { return row; }
+
+    @JsonProperty("col")
+    public int col() { return col; }
+
+    public void setRow(int row) { this.row = row; }
+    public void setCol(int col) { this.col = col; }
+
     public boolean isValid() {
         return row >= 0 && row < 8 && col >= 0 && col < 8;
     }
