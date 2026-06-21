7a88ea67e3e219a6c3e2363c44a8083b2fe43820
mkmokka <mkmokka12@gmail.com>
2026-06-21 23:24:26 +0600

    Update WebSocketConfig.java

--- src/main/java/com/chess/config/WebSocketConfig.java
+++ src/main/java/com/chess/config/WebSocketConfig.java
@@ -16,10 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // Render ক্লাউডের HTTPS রিকোয়েস্ট সিকিউরলি পাস করার গ্যারান্টিড মেথড
         registry.addEndpoint("/ws-chess")
                 .setAllowedOriginPatterns("*")
-                .withSockJS()
-                .setClientLibraryUrl("https://jsdelivr.net");
+                .withSockJS();
     }
 }
