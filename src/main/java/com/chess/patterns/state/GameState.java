bdcf026ec911d6859fe3db9fecec495a518eaa2c
mkmokka <mkmokka12@gmail.com>
2026-06-21 22:08:56 +0600

    Update GameState.java

--- src/main/java/com/chess/patterns/state/GameState.java
+++ src/main/java/com/chess/patterns/state/GameState.java
@@ -1,6 +1,9 @@
 package com.chess.patterns.state;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public interface GameState {
+    @JsonProperty("statusDescription")
     String getStatusDescription();
     boolean canMove();
 }
